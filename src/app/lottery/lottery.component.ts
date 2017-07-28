388f36853be7c8f4571f079ac5d3a40fb55df2cc
kimmyc <cummingsk08@gmail.com>
2017-07-28 18:00:52 -0400

    creating lottery component
    creating lottery component
    
    creating lottery component
    
    creating lottery component
    
    creating lottery component
    
    creating lottery component

--- src/app/lottery/lottery.component.ts
+++ src/app/lottery/lottery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lottery',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lottery.component.css']
 })
 export class LotteryComponent implements OnInit {
+  public lotteryForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required],
+    phone: ['', Validators.required],
+    preferredContact: ['', Validators.required],
+    monday: [false, Validators.required],
+    tuesday: [false, Validators.required],
+    wednesday: [false, Validators.required],
+    thursday: [false, Validators.required],
+    friday: [false, Validators.required]
 
-  constructor() { }
-
-  ngOnInit() {
+  });
+  constructor(public fb: FormBuilder) { }
+  submitLottery(event) {
+    //this will display a json object with the form => we need this!!!
+    console.log(this.lotteryForm.value);
   }
 
+  ngOnInit() {
+
+  }
 }
