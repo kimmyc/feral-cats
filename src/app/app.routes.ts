d802843492af82b92248b70cfd4cac50cca7ef7c
Kimmy <kimmy.cummings@capitalone.com>
2017-07-28 16:19:50 -0400

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+export const APP_ROUTES: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: '**', component: HomeComponent }
+];
