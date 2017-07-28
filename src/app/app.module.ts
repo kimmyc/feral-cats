363193af39bd9227d78836cc408f90027e7c3679
kimmyc <cummingsk08@gmail.com>
2017-07-28 16:55:34 -0400

    generating shell components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { APP_ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LotteryComponent } from './lottery/lottery.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LotteryComponent,
+    AdminLoginComponent,
+    ThankYouComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LotteryComponent } from './lottery/lottery.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
 export const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent},
+  { path: 'admin-login', component: AdminLoginComponent},
+  { path: 'thank-you', component: ThankYouComponent},
+  { path: 'lottery', component: LotteryComponent},
   { path: '**', component: HomeComponent }
 ];
